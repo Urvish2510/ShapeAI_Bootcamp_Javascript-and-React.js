48c826bd50ef640be09ecbd87fa7cc642c92c48d
Urvish Patel <86527895+Urvish2510@users.noreply.github.com>
2021-06-26 21:25:55 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>JavaScript and React.js</h1>
+      <p>A basic web dev React Js bootcamp. This is the project of bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
